4c70fab291bb50fdd62e6581608e9cfba66f03a3
duruaydogdu <duruaydogdu02@gmail.com>
2025-06-15 14:42:42 +0300

    feat: Integrate Kingfisher for image loading with UIImageView extension

--- LoodosCase/Core/Extensions/UIImageView+Kingfisher.swift
+++ LoodosCase/Core/Extensions/UIImageView+Kingfisher.swift
@@ -5,4 +5,21 @@
 //  Created by Duru Aydoğdu on 15.06.2025.
 //
 
-import Foundation
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+    func setImage(with urlString: String?, placeholder: UIImage? = nil) {
+        guard let urlString = urlString,
+              let url = URL(string: urlString) else {
+            self.image = placeholder
+            return
+        }
+
+        self.kf.setImage(
+            with: url,
+            placeholder: placeholder,
+            options: [.transition(.fade(0.2))]
+        )
+    }
+}
